b7e3f5c826924e595fcf43443c9e0ce302fa5374
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-08 09:54:04 +0545

    Variable declarations

---
+++ go.mod
@@ -0,0 +1,8 @@
+module go-practice
+
+go 1.13
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/jinzhu/gorm v1.9.16
+)
---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 10
+	var j int = 11
+	var k int
+	k = 12
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+}
