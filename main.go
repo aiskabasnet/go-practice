4355f93fae7bfe53bbb19c049d6b10f97f48eb21
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-08 09:58:43 +0545

    Added constants

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 10
+	var j int = 11
+	var k int
+	k = 12
+	const a int = 14
+	const b bool = true
+	fmt.Println(i)
+	fmt.Println(j)
+	fmt.Println(k)
+	fmt.Println(a)
+	fmt.Println(b)
+}
