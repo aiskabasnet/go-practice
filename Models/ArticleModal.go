6367e51fa4dca911bf77bc1a7691e8646d78ad8c
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-09 19:03:45 +0545

    Added gorm models

--- Models/ArticleModal.go
+++ Models/ArticleModal.go
@@ -1,9 +1,13 @@
 package Models
 
+import (
+	"github.com/jinzhu/gorm"
+)
+
 //Article => returns article struct
 type Article struct {
-	ID    uint   `json:"id"`
-	Title string `json:"title"`
+	gorm.Model
+	Title string `json:"title" gorm:"size:50"`
 	Desc  string `json:"description"`
 }
 
