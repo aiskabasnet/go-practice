301aba3e925109ca06740bd9f727068e6bc2cd89
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-21 19:24:12 +0545

    Created logger interface

--- main.go
+++ main.go
@@ -1,12 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"go-practice/Routes"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
 )
+
+type CustomString string
+
+type MyLogger struct {
+	Prefix string
+}
+
+func (lg *MyLogger) LogMe(data string) string {
+	return fmt.Sprintf("%v:%v\n", lg.Prefix, data)
+}
+
+type LoggerInterface interface {
+	LogMe(str string) string
+}
 
 func main() {
 	err := godotenv.Load()
