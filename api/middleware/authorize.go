5d6b44c4de7c2a186423625436ed6092b058f45b
Aiska Basnet <aiska.basnet@gmail.com>
2020-12-25 14:43:27 +0545

    Updated auth middleware

--- api/middleware/authorize.go
+++ api/middleware/authorize.go
@@ -2,42 +2,28 @@ package middleware
 
 import (
 	"fmt"
-	"go-practice/handler"
+	"strings"
 	"net/http"
-
-	"github.com/dgrijalva/jwt-go"
+	"go-practice/api/service"
 	"github.com/gin-gonic/gin"
 )
 
-func Authorize() gin.HandlerFunc {
+func Auth(s service.FirebaseService) gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		const BearerSchema string = "Bearer "
-		authHeader := ctx.GetHeader("Authorization")
+		header := ctx.GetHeader("Authorization")
 		if authHeader == "" {
 			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "No authorization header"})
 		}
-		tokenString := authHeader[len(BearerSchema):]
-		if token, err := handler.ValidateToken(tokenString); err != nil {
-
-			fmt.Println("token", tokenString, err.Error())
-			ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{
-				"error": "Not Valid Token"})
-
-		} else {
-
-			if claims, ok := token.Claims.(jwt.MapClaims); !ok {
-				ctx.AbortWithStatus(http.StatusUnauthorized)
-
-			} else {
-				if token.Valid {
-					ctx.Set("userID", claims["userID"])
-					fmt.Println("during authorization", claims["userID"])
-				} else {
-					ctx.AbortWithStatus(http.StatusUnauthorized)
-				}
-
-			}
+		idToken := strings.TrimSpace(strings.Replace(header, "Bearer","",1))
+		token, err := s.VerifyToken(idToken)
+		if err != nil{
+			c.JSON(http.StatusUnauthorized, err.Error())
+			c.Abort()
+			return
 		}
+		c.Set("ID", token.UID)
+		c.Next()
 
 	}
 }
